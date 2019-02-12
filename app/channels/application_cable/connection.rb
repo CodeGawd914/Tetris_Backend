e93e274a653f079b9844bddceac2a3cb5460404e
github username <github email address>
2019-02-12 17:57:04 -0500

    added action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,10 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
-    #
-    # def connect
-    #
-    # end
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_user
+    end
+
+    def find_user
+      if verified_user = User.find_by(id: JWT.decode(request.params[:token], 'my_s3cr3t')[0]["user_id"])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+
+    end
 
   end
 end
---
+++ app/channels/multiGame_channel.rb
@@ -0,0 +1,5 @@
+class MultiGameChannel < ApplicationCable::Channel
+  def subscribed
+
+  end
+end
